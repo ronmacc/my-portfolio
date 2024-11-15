c0c122f033ced4bd2685923566d5f5ee0975188a
aroncal02 <154700667+ronmaccms@users.noreply.github.com>
2024-11-15 13:35:40 -0500

    worked web before adding projects logic

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
                 className="text-[28px] sm:text-[36px] md:text-5xl lg:text-6xl leading-tight"
                 words="Transformed Through Computational Design"
             />
-            <p className="md:tracking-wider text-s md:text-lg lg:text-xl text-blue-100">
+            <p className="uppercase tracking-widest text-s text-blue-100">
                 with code and intuition
             </p>
             <a href="#about">
