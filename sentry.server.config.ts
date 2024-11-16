bf244471c32b7375b66f408d7f6bee1360651fff
aroncal02 <154700667+ronmaccms@users.noreply.github.com>
2024-11-16 16:21:41 -0500

    adding project web working heaby in loading tho

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b82418c9b8bb62552d4f3bbdc8af709b@o4508309309554688.ingest.us.sentry.io/4508309316435968",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
