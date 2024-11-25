80d8491fff0972c47a1b3e2678e63dfa5b828525
aroncal02 <154700667+ronmaccms@users.noreply.github.com>
2024-11-25 16:30:04 -0500

    clean up

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,7 +24,7 @@ const Hero = () => {
                 className="text-[28px] sm:text-[36px] md:text-5xl lg:text-6xl leading-tight"
                 words="Transformed Through Computational Design"
             />
-            <p className="md:tracking-wider text-sm md:text-lg lg:text-xl text-white">
+            <p className="uppercase tracking-widest text-xs text-blue-100">
                 with code and intuition
             </p>
             <a href="#projects">
