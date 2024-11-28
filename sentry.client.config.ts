cc57970d303e3df06d78e68cd14b79a9d1cd4602
aroncal02 <154700667+ronmaccms@users.noreply.github.com>
2024-11-27 22:20:30 -0500

    clean website. punycode module still bug

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,32 +1,11 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://93ccd92c846dc3417afcb3592bf90124@o4508309309554688.ingest.us.sentry.io/4508366184710144",
-
-  // Add optional integrations for additional features
+  dsn: "https://20dc320d164e4a7b419dd1509f03df71@o4508309309554688.ingest.us.sentry.io/4508373436530688",
   integrations: [
-    Sentry.replayIntegration(),
     Sentry.feedbackIntegration({
       // Additional SDK configuration goes in here, for example:
       colorScheme: "dark",
     }),
   ],
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://93ccd92c846dc3417afcb3592bf90124@o4508309309554688.ingest.us.sentry.io/4508366184710144",
+  dsn: "https://20dc320d164e4a7b419dd1509f03df71@o4508309309554688.ingest.us.sentry.io/4508373436530688",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
